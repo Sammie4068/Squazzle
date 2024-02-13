96a9d99019f1fab163acdd3f37fb684acd250431
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-02-13 19:53:52 +0100

    add hero section with responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@
 const navbar = document.getElementById("navbar");
 const navLinks = document.querySelectorAll("#nav_link");
 const menuToggleBtn = document.getElementById("nav_toggle_btn");
+const overlay = document.querySelector(".overlay");
 
 menuToggleBtn.addEventListener("click", () => {
   navbar.classList.toggle("active");
