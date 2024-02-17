86d7796658136d864da870a7a66da651070de7fe
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-02-17 08:19:31 +0100

    add signup page

---
+++ assets/js/signin.js
@@ -0,0 +1,16 @@
+// Password show and hide
+const eyeIcon = document.getElementById("eyeIcon");
+const passwordInput = document.getElementById("passwordInput");
+
+function showpass(input, icon) {
+  if (input.value == "") return;
+  if (input.type == "password") {
+    input.type = "text";
+    icon.innerHTML = `<i class="fas fa-eye-slash"></i>`;
+  } else {
+    input.type = "password";
+    icon.innerHTML = `<i class="fas fa-eye"></i>`;
+  }
+}
+
+eyeIcon.addEventListener("click", () => showpass(passwordInput, eyeIcon));
---
+++ assets/js/signup.js
@@ -0,0 +1,6 @@
+const phoneInput = document.getElementById("phone");
+window.intlTelInput(phoneInput, {
+  utilsScript:
+    "https://cdn.jsdelivr.net/npm/intl-tel-input@19.2.19/build/js/utils.js",
+});
+
