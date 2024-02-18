b738e8682f847044ca0d93fdb05cf064afdd4a3c
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-02-18 22:01:47 +0100

    reconfigure phone country icon to automatically select by user's location

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,5 +1,12 @@
 const phoneInput = document.getElementById("phone");
 window.intlTelInput(phoneInput, {
+  initialCountry: "auto",
+  geoIpLookup: (callback) => {
+    fetch("https://ipapi.co/json")
+      .then((res) => res.json())
+      .then((data) => callback(data.country_code))
+      .catch(() => callback("us"));
+  },
   utilsScript:
     "https://cdn.jsdelivr.net/npm/intl-tel-input@19.2.19/build/js/utils.js",
 });
