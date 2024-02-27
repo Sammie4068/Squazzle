84cb6e99ed0b685177b8a10f39417dba73b330ee
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-02-27 15:23:57 +0100

    add delete modal

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -18,3 +18,38 @@ const dropdownWrapper = document.querySelector(".dropdown__wrapper");
 profileImg.addEventListener("click", () => {
   dropdownWrapper.classList.toggle("hidden");
 });
+
+const navbar = document.getElementById("navbar");
+const navLinks = document.querySelectorAll("#nav_link");
+const menuToggleBtn = document.getElementById("nav_toggle_btn");
+
+function hamMenu() {
+  navbar.classList.toggle("active");
+  menuToggleBtn.classList.toggle("active");
+}
+
+menuToggleBtn.addEventListener("click", hamMenu);
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", hamMenu);
+});
+
+// Modal
+const modals = document.querySelectorAll("#modal");
+const overlay = document.querySelector(".overlay");
+const deleteModal = document.querySelector(".delete_modal");
+
+function openModal(modal) {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+}
+
+function closeModal() {
+  overlay.classList.add("hidden");
+  modals.forEach((modal) => modal.classList.add("hidden"));
+}
+
+const deleteAcccountBtn = document.querySelector(".delete_account");
+deleteAcccountBtn.addEventListener("click", () => {
+  openModal(deleteModal);
+});
